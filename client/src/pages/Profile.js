3def90cfedfdccfe61c01225bc5eeb192a5714ce
KRD13 <rauchdavis13@gmail.com>
2022-03-19 10:59:40 -0400

    code snapshot 21.5

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,17 +8,16 @@ import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
-
 const Profile = (props) => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam }
+    variables: { username: userParam },
   });
 
   const user = data?.me || data?.user || {};
 
-  // redirect to personal profile page if username is the logged-in user's
+  // redirect to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to="/profile" />;
   }
@@ -30,11 +29,11 @@ const Profile = (props) => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
       </h4>
     );
-  };
-
+  }
 
   return (
     <div>
